85da7d90d62833269292931810242955df555e68
Santiago Alarcon <santiago-alar@hotmail.com>
2021-06-09 13:20:47 -0500

    Rename attributes

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,8 +1,8 @@
 
 public class TennisGame1 implements TennisGame {
     
-    private int m_score1 = 0;
-    private int m_score2 = 0;
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
     private String player1Name;
     private String player2Name;
 
@@ -13,17 +13,17 @@ public class TennisGame1 implements TennisGame {
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            m_score1 += 1;
+            scorePlayer1 += 1;
         else
-            m_score2 += 1;
+            scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
+        if (scorePlayer1 == scorePlayer2)
         {
-            switch (m_score1)
+            switch (scorePlayer1)
             {
                 case 0:
                         score = "Love-All";
@@ -40,9 +40,9 @@ public class TennisGame1 implements TennisGame {
                 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (scorePlayer1 >=4 || scorePlayer2 >=4)
         {
-            int minusResult = m_score1-m_score2;
+            int minusResult = scorePlayer1 - scorePlayer2;
             if (minusResult==1) score ="Advantage player1";
             else if (minusResult ==-1) score ="Advantage player2";
             else if (minusResult>=2) score = "Win for player1";
@@ -52,8 +52,8 @@ public class TennisGame1 implements TennisGame {
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = scorePlayer1;
+                else { score+="-"; tempScore = scorePlayer2;}
                 switch(tempScore)
                 {
                     case 0:
